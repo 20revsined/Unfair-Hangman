cb8552e0c51489cebb3ace41b72757efa853f3e8
20revsined <44682153+20revsined@users.noreply.github.com>
2019-11-26 19:20:21 -0500

    Create UnfairHangman.py

---
+++ UnfairHangman.py
@@ -0,0 +1,119 @@
+import random, sys
+
+class GenerateWord():
+	def __init__(self, number, word):
+		self.number = number
+		self.word = word
+
+	def getWord():
+		WordList = []
+		WordList.append("pig")
+		WordList.append("astroid")
+		WordList.append("forest")
+		WordList.append("sunny")
+		WordList.append("canyon")
+		WordList.append("cheese")
+		WordList.append("tomato")
+		WordList.append("royal")
+		WordList.append("cats")
+		WordList.append("dogs")
+		WordList.append("mountain")
+		WordList.append("waterfall")
+		WordList.append("deal")
+		WordList.append("shout")
+		WordList.append("run")
+		WordList.append("lacrosse")
+		WordList.append("soccer")
+		WordList.append("baseball")
+		WordList.append("guitar")
+		WordList.append("football")
+		WordList.append("hockey")
+		WordList.append("baseball")
+		WordList.append("basketball")
+		WordList.append("salon")
+		WordList.append("salmon")
+		WordList.append("fish")
+		WordList.append("shark")
+		WordList.append("squid")
+		WordList.append("whale")
+		WordList.append("shrimp")
+		WordList.append("abcdefghijklmnopqrstuvwxyz")
+
+		return WordList[random.randint(0, len(WordList) - 1)]
+
+class Guess():
+	global GenerateWord
+	word = GenerateWord.getWord()
+	listWord = list(word)
+
+	x = 0
+	z = 0
+
+	string = ""
+
+	def toString(string):
+		z = ""
+		NewString = ""
+		for z in list(string):
+			NewString += z
+		return NewString
+
+	def encode(string, EncodedString):
+		x = 0
+		while x < len(string):
+			EncodedString += "*"
+			x += 1
+		return EncodedString
+
+	CodedWord = encode(word, string)
+
+	print(CodedWord)
+
+	listCodedWord = list(CodedWord)
+
+	#print(listCodedWord)
+
+	print("Welcome to Unfair Hangman! You will guess letters and try guess the correct word. Good luck!")
+
+	guess = 0
+	GuessLimit = len(word) + 3
+	number = 0
+
+	while True:
+		letter = str(input("Please enter a letter."))
+		number = listWord.count(letter)
+
+		if guess == GuessLimit and "*" in listCodedWord:
+			print("Sorry, you were unable to guess the word. The word was " + word + ".")
+			sys.exit(0)
+
+		if number > 0:
+			while z < len(listCodedWord):
+				if letter == listWord[z]:
+					listCodedWord[z] = letter
+				z += 1
+			print(toString(listCodedWord))
+			#guess += 1
+			z = 0
+
+		else:
+			print(toString(listCodedWord))
+		
+		guess += 1
+
+		if(guess % 5 == 0):
+			word = ""
+			word = GenerateWord.getWord()
+			CodedWord = ""
+			encode(word, CodedWord)
+			#listCodedWord = ""
+			listCodedWord = list(CodedWord)
+			GuessLimit = len(word) + 3
+			print("New Word!\n" + encode(word, CodedWord))
+
+
+		if listCodedWord == listWord and guess <= GuessLimit:
+			print("Congratulations! You guessed the word!")
+			sys.exit(0)
+
+#source: https://www.geeksforgeeks.org/python-convert-list-characters-string/
